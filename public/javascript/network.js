9a64f9ed6fcaf78e240c623b13db94582a247b47
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-05-24 21:32:19 +0000

    Create AJAX function fetch menuItems from db

--- public/javascript/network.js
+++ public/javascript/network.js
@@ -2,7 +2,7 @@
 // Initiates an AJAX request to the server at the URL /api/menuItems
 // using the GET method by default.
 function getAllMenuItems(params) {
-  let url = "/api/menuItems";
+  let url = "/api/users/menuItems";
   if (params) {
     // Indicated an added query to URL, for params
     url += "?" + params;

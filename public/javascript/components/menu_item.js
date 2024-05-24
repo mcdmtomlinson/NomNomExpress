a0e895e960d03336f190dd90d68942f56952a25b
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-05-24 22:46:21 +0000

    update menu_item

--- public/javascript/components/menu_item.js
+++ public/javascript/components/menu_item.js
@@ -4,26 +4,32 @@ $(() => {
   window.menuItem = {};
 
   // Defining the function createMenuItem which adds listing info to html article
-  function createMenuItem(menuItem) {
+  function createMenuItem(item) {
     return `
-    <article class="menu-item">
-        <section class="menu-item__preview-image">
-          <img src="${menuItem.thumbnail_photo_url}" alt="Menu Item">
-        </section>
-        <section class="menu-item__details">
-          <h3 class="menu-item__title">${menu-item.name}</h3>
-          <ul class="menu-item__details">
-            <li>${menuItem.details}</li>
-          </ul>
+      <div><h3> This is the item - ${item} </h3> </div>`
+    // FOR NOW CREATE DUMMY ITEM, THEN CREATE ARTICLE
 
-            : ``}
-          <footer class="menu-item__footer">
+    // <article class="property-listing">
+    //     <section class="property-listing__preview-image">
+    //       <img src="${property.thumbnail_photo_url}" alt="house">
+    //     </section>
+    //     <section class="property-listing__details">
+    //       <h3 class="property-listing__title">${property.title}</h3>
+    //       <ul class="property-listing__details">
+    //         <li>number_of_bedrooms: ${property.number_of_bedrooms}</li>
+    //         <li>number_of_bathrooms: ${property.number_of_bathrooms}</li>
+    //         <li>parking_spaces: ${property.parking_spaces}</li>
+    //       </ul>
+    //       ${isReservation ?
+    //         `<p>${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>`
+    //         : ``}
+    //       <footer class="property-listing__footer">
+    //         <div class="property-listing__rating">${Math.round(property.average_rating * 100) / 100}/5 stars</div>
+    //         <div class="property-listing__price">$${property.cost_per_night/100.0}/night</div>
+    //       </footer>
+    //     </section>
+    //   </article>
 
-            <div class="property-listing__price">$${menuItem/100.0}</div>
-          </footer>
-        </section>
-      </article>
-    `
   }
   // Allowing the function to be accessed globally
   window.menuItem.createMenuItem = createMenuItem;

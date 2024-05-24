ad49d2b47ebe20cd11898c911659d8310d3a73d8
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-05-24 22:45:50 +0000

    Update header

--- public/javascript/components/home.js
+++ public/javascript/components/home.js
@@ -2,18 +2,18 @@ $(() => {
 
   // This is rendering a 'Home Page', with a button for cart
   // Ryan - I moved the title and button to header temporarily
-//   const $home = $(`
-//   <h1>NomNomExpress</h1>
-// <ul>
-//   <button id="cart">cart</button>
-// </ul>
-//   `);
-//   window.$home = $home;
+  const $home = $(`
+  <h1>NomNomExpressHOME</h1>
+<ul>
+  <button id="cart">cart</button>
+</ul>
+  `);
+  window.$home = $home;
 
-//   $home.on('click', '#cart',function() {
-//     console.log('here');
-//     views_manager.show('cart');
-//   });
+  $home.on('click', '#cart',function() {
+    console.log('here');
+    views_manager.show('cart');
+  });
 
 });
 

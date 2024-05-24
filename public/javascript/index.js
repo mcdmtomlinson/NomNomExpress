ba006f53c75b7f30789beaf54e92ae2d62d50af4
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-05-24 21:31:10 +0000

    Begin to create functionality to append menu items to #main

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,11 +1,15 @@
 $(()=> {
-  views_manager.show('home');
+  // views_manager.show('home');
+  // views_manager.show('menuItems');
+
 
 
   //Calling getAllMenuItems function, defined in network.js.
   // Initiates an AJAX request to the server at the URL /api/menItems?params
   // Returning Promise using json object as param
   getAllMenuItems().then(function( json ) {
+    console.log(json);
+    propertyListings.addProperties(json.properties);
   /// Call global function show from views_manager object, with listings as parameter
   // This will append $propertyListings to #main
     views_manager.show('menuItems');

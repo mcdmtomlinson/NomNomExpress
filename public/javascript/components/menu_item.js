322ef144f0bc425339840187e64eed4758290800
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-05-26 18:46:16 +0000

    Add data-item attribute to html

--- public/javascript/components/menu_item.js
+++ public/javascript/components/menu_item.js
@@ -6,36 +6,15 @@ $(() => {
   // Defining the function createMenuItem which adds listing info to html article
   function createMenuItem(item) {
     return `
-      <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+      <div class="card menu-item" style="width: 18rem;" data-item='${JSON.stringify(item)}'>
+        <img src="${item["photo"]} class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${item}</h5>
-          <p class="card-text">This is the item - ${item}</p>
-          <a href="#" class="btn btn-primary">Add To Cart</a>
+          <h5 class="card-title">${item["name"]}</h5>
+          <p class="card-text">${item["description"]}</p>
+          <p>$${item["price"]}</p>
+          <a href="#" class="btn btn-primary add-to-cart">Add To Cart</a>
         </div>
       </div>`
-    // FOR NOW CREATE DUMMY ITEM, THEN CREATE ARTICLE
-
-    // <article class="property-listing">
-    //     <section class="property-listing__preview-image">
-    //       <img src="${property.thumbnail_photo_url}" alt="house">
-    //     </section>
-    //     <section class="property-listing__details">
-    //       <h3 class="property-listing__title">${property.title}</h3>
-    //       <ul class="property-listing__details">
-    //         <li>number_of_bedrooms: ${property.number_of_bedrooms}</li>
-    //         <li>number_of_bathrooms: ${property.number_of_bathrooms}</li>
-    //         <li>parking_spaces: ${property.parking_spaces}</li>
-    //       </ul>
-    //       ${isReservation ?
-    //         `<p>${moment(property.start_date).format('ll')} - ${moment(property.end_date).format('ll')}</p>`
-    //         : ``}
-    //       <footer class="property-listing__footer">
-    //         <div class="property-listing__rating">${Math.round(property.average_rating * 100) / 100}/5 stars</div>
-    //         <div class="property-listing__price">$${property.cost_per_night/100.0}/night</div>
-    //       </footer>
-    //     </section>
-    //   </article>
 
   }
   // Allowing the function to be accessed globally
